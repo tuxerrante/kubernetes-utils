f9c7a793f6842586c20ca9995eca0a0d8cf72b5d
affinito <alessandro.affinito@sisal.it>
2021-08-30 18:42:31 +0200

    init-master

--- local-install/multipass/multipass-init-worker.sh
+++ local-install/multipass/multipass-init-worker.sh
@@ -19,12 +19,6 @@ sudo rm -f /etc/resolv.conf
 sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sudo systemctl restart systemd-resolved.service
 
-# Update your dns based on your laptop network
-# sudo tee /etc/resolv.conf <<EOF
-# nameserver 10.34.168.6
-# nameserver 8.8.8.8
-# EOF
-
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl
 
@@ -64,32 +58,3 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 
 sudo swapoff -a
 
-# https://kubernetes.io/docs/reference/config-api/kubelet-config.v1beta1/
-cat > kubeadm-config.yaml <<EOF 
-kind: ClusterConfiguration
-apiVersion: kubeadm.k8s.io/v1beta3
-kubernetesVersion: v1.22.1
----
-kind: KubeletConfiguration
-apiVersion: kubelet.config.k8s.io/v1beta1
-cgroupDriver: systemd
-podCIDR: 10.244.0.0/16
-EOF
-
-# sudo kubeadm init --pod-network-cidr=10.244.0.0/16 
-sudo kubeadm init --config kubeadm-config.yaml
-
-mkdir -p $HOME/.kube
-sudo cp --force /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-#--- Flannel CNI
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-# https://github.com/flannel-io/flannel/issues/1344#issuecomment-867265435
-kubectl patch node $(hostname) -p '{"spec":{"podCIDR":"10.244.0.0/16"}}'
-kubectl delete pod -n kube-system -l app=flannel
-
-############################
-#### RESET 
-# kubectl config delete-cluster kubernetes && sudo kubeadm reset && kubectl delete node master-k8s && sudo rm -rf /etc/cni/net.d
--- local-install/multipass/multipass-init.md
+++ local-install/multipass/multipass-init.md
@@ -8,20 +8,13 @@ multipass launch --name worker-1-k8s --cpus 2 --mem 2048M --disk 5G
 multipass launch --name worker-2-k8s --cpus 2 --mem 2048M --disk 5G
 ```
 
-While on VPN
-```
-multipass launch --name worker-1-k8s --cpus 2 --mem 2048M --disk 5G --network
-```
 
 Exec init commands
+10.34.168.6 Will be my DNS for my VPN connection
 ```
 multipass exec master-k8s -- bash -c "echo nameserver 10.34.168.6 | sudo tee /etc/resolv.conf  && curl -sO https://raw.githubusercontent.com/tuxerrante/kubernetes-utils/main/local-install/multipass/multipass-init-masterk8s.sh  && chmod +x multipass-init-masterk8s.sh  && ./multipass-init-masterk8s.sh"
 
 ```
-Not working:
-  Get-Content .\multipass-init-masterk8s.sh | multipass transfer -v - master-k8s:$HOME/init-masterk8s.sh
-  multipass exec master-k8s multipass-init-masterk8s.sh
-
 
 Add workers to the cluster
 ```
